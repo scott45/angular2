14bb3c74d2abd5b63c485f780c2428609c34ca3c
scotty-b <businge.scott@andela.com>
2017-07-21 18:39:44 +0300

    videos class

---
+++ app/ts/videos.ts
@@ -0,0 +1,14 @@
+export class videos{
+    id:number;
+    title:string;
+    code:number;
+    description:string;
+
+    constructor(id:number, title:string, code:number, description:string){
+        this.id=id;
+        this.title=title;
+        this.code=code;
+        this.description=description;
+    }
+
+}
